261d1d6a3810e3e8e40b8deaa0b2e2138003d414
iamsharonz <sharonjoseph3339@gmail.com>
2022-11-28 21:21:03 +0530

    bots custom message/replies

---
+++ script.py
@@ -0,0 +1,111 @@
+class Script(object):
+
+    START_MSG = """<b>Hello</b> **{}**,
+
+<b>I'm an  advanced 🄵🄸🄻🅃🄴🅁 🅁🄾🄱🄾🅃 exclusively made for Filtering Purposes!!</b>
+
+<b>Feel free to add me in your group :)</b>
+
+<b>Click HELP to see what I can do!!.</b>
+
+© **@Nobodys_Bots**
+"""
+
+
+    
+    HELP_MSG = """
+    
+<b>Usual commands;</b>
+<i>/start</i> - Check Whether I'm Online 💯
+<i>/about</i> - About Meh! 🥱
+
+<b>Filters;</b>
+<i>/add</i> <filter name> <reply> - Add a filter 
+<i>/viewfilters</i> - List of all the filters of a chat
+<i>/del</i> <filter name> - Delete the specified filter
+<i>/delall</i> - Delete the whole filters in a chat (chat owner only)
+
+<b>Connections;</b>
+<i>/connect</i> <chat id> - Connect a particular chat to your PM
+<i>/disconnect</i> <chat id> - Disconnect from a chat
+<i>/connections</i> - List of all your connections
+
+<b>Utilities;</b>
+<i>/id</i> - To get the chat id
+<i>/info</i> <user id> or reply to any message - To get the whois info of a user
+ 
+ © **@Nobodys_Bots**
+ """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ABOUT_MSG = """⭕️<b>My Name :</b> 𝕌ℕ𝕃𝕀𝕄𝕀𝕋𝔼𝔻 𝔽𝕀𝕃𝕋𝔼ℝ ℝ𝕆𝔹𝕆𝕋
+
+⭕️<b>Creater :</b> <a href='https://t.me/MrNobody_Here'>ᬊꛘ᭵ᱠ᭵᠔ᥙᩛ᭢</a> 
+
+⭕️<b>Server :</b> AWS
+
+⭕️<b>Source :</b> Scrapped from <a href='https://github.com/PaulSonOfLars/tgbot'>Marie</a>
+
+"""
